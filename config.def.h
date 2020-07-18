3e021c2cd76d040bebaa3bff4e970fa98abaf17c
Shourai <10200748+Shourai@users.noreply.github.com>
2020-07-18 12:56:05 +0200

    Scroll back 10 lines instead of a whole page

--- config.def.h
+++ config.def.h
@@ -8,8 +8,8 @@
 
 struct rule rules[] = {
 	/* sequence     event        lines */
-	{"\033[5;2~",   SCROLL_UP,   -1},       /* [Shift] + [PageUP] */
-	{"\033[6;2~",   SCROLL_DOWN, -1},       /* [Shift] + [PageDown] */
+	{"\033[5;2~",   SCROLL_UP,   10},       /* [Shift] + [PageUP] */
+	{"\033[6;2~",   SCROLL_DOWN, 10},       /* [Shift] + [PageDown] */
 	/* mouse binding shadows ^E and ^Y, so it's disabled by default */
 	{"\031",        SCROLL_UP,    5},       /* mouse wheel up */
 	{"\005",        SCROLL_DOWN,  5},       /* mouse wheel Down */
